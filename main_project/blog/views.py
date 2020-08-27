1a7f97a7ed9d61229657b25aa3a1d84cfbc58671
xcript1 <o_olatunji@mail.harper.edu>
2020-08-26 21:42:45 -0500

    commit

--- main_project/blog/views.py
+++ main_project/blog/views.py
@@ -1,27 +1,12 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'David',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'August 26, 2020'
-    },
-    {
-        'author': 'Davide',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date_posted': 'August 27, 2020'
-    }
-]
+from .models import Post
 
 
-# Create your views here.
 def home(request):
-    contex = {
-        'posts': posts
+    context = {
+        'posts': Post.objects.all()
     }
-    return render(request, 'blog/home.html', contex)
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
