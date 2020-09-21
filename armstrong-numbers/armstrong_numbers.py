5f5bb26c8bbc796771b290306bcac8fa7286749e
xcript1 <o_olatunji@mail.harper.edu>
2020-09-21 13:52:20 -0500

    commit

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,12 +1,12 @@
 def is_armstrong_number(number):
-    sum = 0
-    length = len(str(number))
-    temp = number
+    sum = 0     #initialize sum
+    length = len(str(number)) #initialize length of number
+    temp = number   #set temp to numer
 
     while temp > 0:
-        digit = temp % 10
-        sum += digit ** length
-        temp //= 10
+        digit = temp % 10   #modulo of number will be last digit of number
+        sum += digit ** length  #add last digit to the answer and divide each by length
+        temp //= 10     #number divided by 10
 
     if number == sum:
         return True
