9e29362d6393f48a6a2b3526712a0ad05db0392a
xcript1 <o_olatunji@mail.harper.edu>
2020-09-23 20:12:15 -0500

    comiit

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -1,11 +1,8 @@
 def is_pangram(sentence):
-    import string
-    sentence = sentence.lower()
-    alphabet = [char for char in string.ascii_lowercase]
-    for x in sentence:
-        if x in alphabet:
-            alphabet.remove(x)
-    if len(alphabet) > 0:
-        return False
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    for letter in alphabets:
+        if letter not in sentence.lower():
+            return False
+        
     return True
     
